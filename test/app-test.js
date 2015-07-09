26c53436d89937106c6643674db77dd9ffe0549f
gonzalovazquez <gonzalo.segura1@gmail.com>
2015-07-08 20:56:41 -0400

    added passing tests

--- test/app-test.js
+++ test/app-test.js
@@ -1,74 +1,81 @@
 'use strict';
 
 var path = require('path');
-//require('jasmine-beforeall');
+var rimraf = require('rimraf');
 var assert = require('yeoman-generator').assert;
 var helpers = require('yeoman-generator').test;
-var rimraf = require('rimraf');
-var tmpDir = path.join( __dirname, './testApp');
 
 describe('generator-rna:app', function () {
 
-  describe('generator-rna angular', function() {
-    before(function (done) {
-      helpers.run(path.join(__dirname, '../app'))
-        .inDir(tmpDir)
-        .withOptions({ skipInstall: true })
-        .withPrompts({ appName: 'testApp' })
-        .withPrompts({ appType: 'AngularJS' })
-        .on('end', done);
-    });
 
-    it('should create proper scaffolding', function () {
-      assert.file([
-        'testApp/bower.json',
-        'testApp/package.json',
-        'testApp/.gitignore',
-        'testApp/src/js/app.js',
-        'testApp/src/styles/main.css'
-      ]);
-    });
+  describe('generator-rna: angular', function() {
 
-    // it('should print out app name in bower and package', function() {
-    //   assert.fileContent('testApp/bower.json',  /"name": "testApp"/);
-    //   assert.fileContent('testApp/package.json',  /"name": "testApp"/);
-    // });
+      before(function (done) {
+        helpers.run(path.join(__dirname, '../app'))
+          .inDir(path.join(__dirname, '.testApp'))
+          .withOptions({ skipInstall: true })
+          .withPrompts({ appName: 'testApp' })
+          .withPrompts({ appType: 'AngularJS' })
+          .on('end', done);
+      });
 
-    // it('should install angular dependency', function() {
-    //   assert.fileContent('testApp/bower.json',  /"dependencies": "angular"/);
-    // });
+      it('can be required without throwing', function() {
+        this.app = require('../app');
+      });
 
-    // it('should include angular dependency', function() {
-    //   assert.fileContent('testApp/src/index.html', /"script": "angular"/);
-    // });
+      it('should create proper scaffolding', function () {
+          assert.file([
+            'bower.json',
+            'package.json',
+            '.gitignore',
+            'src/js/app.js',
+            'src/styles/main.css'
+          ]);
+      });
 
-    afterEach(function(){
-      process.chdir(tmpDir+'/../');
-      rimraf.sync(tmpDir);
-    });
+      it('should print out app name in bower and package', function() {
+        assert.fileContent('bower.json',  /"name": "testApp"/);
+        assert.fileContent('package.json',  /"name": "testApp"/);
+      });
+
+      it('should install angular dependency', function() {
+        assert.fileContent('bower.json', new RegExp('"angular"'));
+      });
+
+      it('should include angular dependency', function() {
+        assert.fileContent('src/index.html', new RegExp('<script type="text/javascript" src="/bower_components/angular/angular.js"></script>'));
+      });
+
+      afterEach(function() {
+        rimraf.sync('testApp');
+      });
   });
 
-  // describe('generator-rna react', function() {
-  //   before(function (done) {
-  //     helpers.run(path.join(__dirname, '../app'))
-  //       .withOptions({ skipInstall: true })
-  //       .withPrompts({ appName: 'app' })
-  //       .withPrompts({ appType: 'ReactJS' })
-  //       .on('end', done);
-  //   });
+    before(function (done) {
+        helpers.run(path.join(__dirname, '../app'))
+          .inDir(path.join(__dirname, '.testApp'))
+          .withOptions({ skipInstall: true })
+          .withPrompts({ appName: 'testApp' })
+          .withPrompts({ appType: 'ReactJS' })
+          .on('end', done);
+      });
 
-  //   it('should print out app name in bower and package', function() {
-  //     assert.fileContent('app/bower.json',  /"name": "app"/);
-  //     assert.fileContent('app/package.json',  /"name": "app"/);
-  //   });
+      it('should print out app name in bower and package', function() {
+        assert.fileContent('bower.json',  /"name": "testApp"/);
+        assert.fileContent('package.json',  /"name": "testApp"/);
+      });
 
-  //   it('should install react dependency', function() {
-  //     assert.fileContent('app/bower.json',  /"dependencies": "react"/);
-  //   });
+      it('should install react dependency', function() {
+        assert.fileContent('bower.json', new RegExp('"react"'));
+      });
 
-  //   it('should include react dependency', function() {
-  //     assert.fileContent('app/src/index.html', /"script": "react"/);
-  //   });
-  // });
-  
+      it('should include react dependency', function() {
+        assert.fileContent('src/index.html', new RegExp('<script type="text/javascript" src="/bower_components/react/react.js"></script>'));
+      });
+
+      afterEach(function() {
+        rimraf.sync('testApp');
+      });
+
 });
+
